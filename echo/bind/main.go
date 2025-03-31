8ba9edb5b3ed135207bf048170979477ce5a0d0b
Hasushi <hasuprog1225@gmail.com>
2025-03-31 19:13:01 +0900

    bind

---
+++ echo/bind/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+)
+
+type User struct {
+	Name string `json:"name" query:"name"`
+	Age  int    `json:"age" query:"age"`
+}
+
+func main(){
+	e := echo.New()
+	logger := e.Logger
+	logger.SetLevel(log.INFO)
+	logger.SetHeader("${time_rfc3339} ${level} ${short_file}:${line} ${message}")
+
+	// 普通のルート
+	e.GET("/", func(c echo.Context) error {
+		logger.Infof("Hello, World!")
+		return c.String(200, "Hello, World!")
+	})
+
+	// クエリパラメータ
+	// curl -X GET "http://localhost:8080/query?name=Hasushi&age=21"
+	e.GET("/query", func(c echo.Context) error {
+		user := User{}
+		if err := c.Bind(&user); err != nil {
+			return c.String(400, "Bad Request")
+		}
+		logger.Infof("Hello, %s! You are %d years old.", user.Name, user.Age)
+		return c.String(200, "Hello, "+user.Name+"! You are "+string(rune(user.Age))+" years old.")
+	})
+	// JSONパラメータ
+	// curl -X POST -H "Content-Type: application/json" -d '{"name":"Hasushi","age":21}' "http://localhost:8080/json"
+	e.POST("/json", func(c echo.Context) error {
+		user := User{}
+		if err := c.Bind(&user); err != nil {
+			return c.String(400, "Bad Request")
+		}
+		logger.Infof("Hello, %s! You are %d years old.", user.Name, user.Age)
+		return c.String(200, "Hello, "+user.Name+"! You are "+string(rune(user.Age))+" years old.")
+	})
+
+	logger.Fatal(e.Start(":8080"))
+	
+}
