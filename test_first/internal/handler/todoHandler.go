cf88de4d7c7abaa458fd3934e8ccc13d89fe1141
IkejimaRen <hasuprog1225@gmail.com>
2025-04-26 20:49:48 +0900

    implement: todoHandler

--- test_first/internal/handler/todoHandler.go
+++ test_first/internal/handler/todoHandler.go
@@ -1,10 +1,23 @@
 package handler
 
 import (
+	"net/http"
+	"todo/internal/entity"
+
 	"github.com/labstack/echo/v4"
 )
 
 func CreateTodo(c echo.Context) error {
+	var todo entity.Todo
+	if err := c.Bind(&todo); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "bind error"})
+	}
+	if err := c.Validate(&todo); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "validation error"})
+	}
+
+	
+
 	panic("not implemented")
 }
 
