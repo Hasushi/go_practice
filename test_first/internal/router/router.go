f2864781c50dcae5020bb6ce086ff7b2b818db8b
IkejimaRen <hasuprog1225@gmail.com>
2025-04-26 20:11:13 +0900

    fix: router

--- test_first/internal/router/router.go
+++ test_first/internal/router/router.go
@@ -4,10 +4,15 @@ import (
 	"todo/internal/handler"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter() *echo.Echo {
 	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.GET("/todos", handler.GetTodos)
 
