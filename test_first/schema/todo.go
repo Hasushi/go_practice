0bf6eb7fd0f6df318d6a035b113b0ff1f57512fb
IkejimaRen <hasuprog1225@gmail.com>
2025-04-28 19:50:14 +0900

    add schema

---
+++ test_first/schema/todo.go
@@ -0,0 +1,10 @@
+package schema
+
+type Todo struct {
+	ID 		int    `json:"id"`
+	Title 	string `json:"title" validate:"required"`
+	Description string `json:"description"`
+	Completed bool   `json:"completed"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
