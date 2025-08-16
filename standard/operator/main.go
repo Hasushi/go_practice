9d81e6b931a97297c6187415108163e0aeaaacc9
Hasushi <hasuprog1225@gmail.com>
2025-08-17 00:10:52 +0900

    practice: bit operation

--- standard/operator/main.go
+++ standard/operator/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	Sample2()
+}
+
+func Sample1() {
 	var x uint8 = 200
 	fmt.Printf("x: %v\n", x)
 	fmt.Printf("x(binary): %08b\n", x)
@@ -18,4 +22,18 @@ func main() {
 	w := ^z + 1 // 二の補数
 	fmt.Printf("w: %v\n", w)
 	fmt.Printf("w(binary): %08b\n", w)
+}
+
+func Sample2() {
+	var x uint8 = 200
+	fmt.Printf("x: %v\n", x)
+	fmt.Printf("x(binary): %08b\n", x)
+
+	var y uint8 = 100
+	fmt.Printf("y: %v\n", y)
+	fmt.Printf("y(binary): %08b\n", y)
+
+	z := x &^ y // ビットAND NOT
+	fmt.Printf("z: %v\n", z)
+	fmt.Printf("z(binary): %08b\n", z)
 }
